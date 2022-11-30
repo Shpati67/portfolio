62da9e46c730b00dd0afd69dd72a9d9a38576e45
Shpati67 <118909423+Shpati67@users.noreply.github.com>
2022-11-30 14:36:46 +0100

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const skillItems = Array.from(document.getElementsByClassName('skill-box'));
-
-skillItems.forEach((skillItem) => {
-  const width = skillItem.getAttribute('skill-value');
-  const skillValueSpan = skillItem.querySelector('.percent-text');
-  skillValueSpan.innerText = `${width}%`;
-  const skillbar = skillItem.querySelector('.skill-bar');
-  skillbar.style.width = `${width}%`;
-});
